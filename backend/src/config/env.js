54f9780146e87c2af4f62e708d8f4ff85ec930da
Jaliya VOX <141953235+jaliyavox@users.noreply.github.com>
2026-04-29 22:09:17 +0530

    Update env.js

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const dotenv = require('dotenv');
 
-// ✅ Change to this
+
 dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 
 function requireEnv(name) {
